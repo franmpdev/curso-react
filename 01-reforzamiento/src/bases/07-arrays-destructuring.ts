1186f47b23e13d013199d0e76f8ec0edff834fa7
franmpdev <franmorenopecofmp@gmail.com>
2025-09-18 03:06:42 +0200

    Ejercicio práctico basado en un Hook de React, UseState

--- 01-reforzamiento/src/bases/07-arrays-destructuring.ts
+++ 01-reforzamiento/src/bases/07-arrays-destructuring.ts
@@ -8,3 +8,16 @@ const returnsarraysfn = () => {
 }
 const [letters,number] = returnsarraysfn();
 console.log({letters, number});
+
+//EJERCICIO BASADO EN UN HOOK DE REACT, USESTATE
+const useState = (data: string)=>{
+    return [data, (newData: string)=>{
+        data = newData;
+        console.log(data);
+    }] as const;
+
+}
+
+const [ name, setName ] = useState('Goku');
+console.log(name);
+setName('Vegeta');
