b7bdc674a24c88d5415279cd60673abb22f1229c
franmpdev <franmorenopecofmp@gmail.com>
2025-09-18 18:06:48 +0200

    Clase de enumeraciones e interfaces. Diferencia entre tipos y enums.

---
+++ 01-reforzamiento/src/data/heroes-data.ts
@@ -0,0 +1,21 @@
+interface Hero {
+    id: number;
+    name: string;
+    owner: Owner;
+}
+enum Owner {
+    DC = 'DC',
+    Marvel = 'Marvel',
+}
+//type Owner = 'DC' | 'Marvel';
+const heroes: Hero[] = [
+    { id: 1, name: 'Batman', owner: Owner.DC },
+    { id: 2, name: 'Spiderman', owner: Owner.Marvel },
+    { id: 3, name: 'Superman', owner: Owner.DC },
+    { id: 4, name: 'Flash', owner: Owner.DC },
+    { id: 5, name: 'Wolverine', owner: Owner.Marvel },
+    { id: 6, name: 'Thor', owner: Owner.Marvel },
+    { id: 7, name: 'Green Lantern', owner: Owner.DC },
+    { id: 8, name: 'Green Arrow', owner: Owner.DC },
+]
+
