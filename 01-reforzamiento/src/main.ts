37db83cb333c8fc75db85367af8f37110016c971
franmpdev <franmorenopecofmp@gmail.com>
2025-09-21 02:06:32 +0200

    Clase async-await. Uso del async y await para declarar funciones asíncronas y peticiones a API

--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -6,7 +6,7 @@ import './bases/06-obj-destructuring'
 import './bases/07-arrays-destructuring'
 import './bases/08-imp-exp'
 import './bases/09-promises'
-import './bases/10-fetch-api'
+import './bases/11-async-await'
 //EJERCICIO
 import { getHeroesByOwner } from './bases/08-imp-exp'
 import { Owner } from './data/heroes-data'
